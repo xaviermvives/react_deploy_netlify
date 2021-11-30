4fcf6cddadf71b346b6170c7e796e661fb9a0140
Xavier <montjuic79@gmail.com>
2021-11-30 10:44:18 +0100

    useRef to focus input after submit

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useRef } from "react";
 import { FaPlus } from "react-icons/fa";
 
 const AddItem = ({ newItem, setNewItem, handleSubmit }) => {
+  const inputRef = useRef();
+
   return (
     <form className="addForm" onSubmit={handleSubmit}>
       <label htmlFor="addItem">Add Item</label>
       <input
         autoFocus
+        ref={inputRef}
         id="addItem"
         type="text"
         placeholder="AddItem"
@@ -14,7 +17,11 @@ const AddItem = ({ newItem, setNewItem, handleSubmit }) => {
         value={newItem}
         onChange={(e) => setNewItem(e.target.value)}
       />
-      <button type="submit" aria-label="Add Item">
+      <button
+        type="submit"
+        aria-label="Add Item"
+        onClick={() => inputRef.current.focus()}
+      >
         <FaPlus />
       </button>
     </form>
