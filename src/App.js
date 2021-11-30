9ebf2f6232e6abce8fdc020a4b2518b5ce6cf878
Xavier <montjuic79@gmail.com>
2021-11-30 10:08:26 +0100

    Add Item

--- src/App.js
+++ src/App.js
@@ -1,45 +1,82 @@
-import React, {useState} from 'react';
-import Content from './Content';
-import Footer from './Footer';
-import Header from './Header';
+import React, { useState } from "react";
+import AddItem from "./AddItem";
+import Content from "./Content";
+import Footer from "./Footer";
+import Header from "./Header";
 
 function App() {
   const [items, setItems] = useState([
     {
-        id: 1,
-        checked: true,
-        item: "One half pound bag of Cocoa Covered Almonds Unsalted"
+      id: 1,
+      checked: true,
+      item: "One half pound bag of Cocoa Covered Almonds Unsalted",
     },
     {
-        id: 2,
-        checked: false,
-        item: "Item 2"
+      id: 2,
+      checked: false,
+      item: "Item 2",
     },
     {
-        id: 3,
-        checked: false,
-        item: "Item 3"
-    }
-])
+      id: 3,
+      checked: false,
+      item: "Item 3",
+    },
+  ]);
 
-const handleCheck = (id) => {
-    const listItems = items.map(item => 
-        item.id === id ? {...item, checked: !item.checked} : item
-    )
-    setItems(listItems)
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems))
-}
+  const [newItem, setNewItem] = useState("");
 
-const handleDelete = (id) => {
+  const setAndSaveItems = (newItems) => {
+    setItems(newItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(newItems));
+  };
+
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const myNewItem = {
+      id,
+      checked: false,
+      item,
+    };
+    const listItems = [...items, myNewItem];
+    setAndSaveItems(listItems);
+  };
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setAndSaveItems(listItems);
+  };
+
+  const handleDelete = (id) => {
     console.log(id);
-    const newList = items.filter(item => item.id !== id)
-    setItems(newList)
-}
+    const newList = items.filter((item) => item.id !== id);
+    setAndSaveItems(newList);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+    // console.log(newItem);
+    //addItem
+    addItem(newItem);
+    setNewItem("");
+    // console.log("submitted");
+  };
 
   return (
     <div className="App">
-      <Header  />
-      <Content items={items} checkItem={handleCheck} deleteItem={handleDelete} />
+      <Header />
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
+      <Content
+        items={items}
+        checkItem={handleCheck}
+        deleteItem={handleDelete}
+      />
       <Footer length={items.length} />
     </div>
   );
--- src/LineItem.js
+++ src/LineItem.js
@@ -1,14 +1,28 @@
-import React from 'react'
-import { FaTrashAlt} from 'react-icons/fa'
+import React from "react";
+import { FaTrashAlt } from "react-icons/fa";
 
-const LineItem = ({item, handleDelete, handleCheck}) => {
-    return (
-        <li className="item" key={item.id}>
-        <input type="checkbox" checked={item.checked} onChange={() => handleCheck(item.id)} />
-        <label style={item.checked ? {textDecoration:'line-through'} : null}onDoubleClick={() => handleCheck(item.id)}>{item.item}</label>
-        <FaTrashAlt onClick={() => handleDelete(item.id)}role="button" tabIndex="0" aria-label={`Delete ${item.item}`} />
+const LineItem = ({ item, handleDelete, handleCheck }) => {
+  return (
+    <li className="item">
+      <input
+        type="checkbox"
+        checked={item.checked}
+        onChange={() => handleCheck(item.id)}
+      />
+      <label
+        style={item.checked ? { textDecoration: "line-through" } : null}
+        onDoubleClick={() => handleCheck(item.id)}
+      >
+        {item.item}
+      </label>
+      <FaTrashAlt
+        onClick={() => handleDelete(item.id)}
+        role="button"
+        tabIndex="0"
+        aria-label={`Delete ${item.item}`}
+      />
     </li>
-    )
-}
+  );
+};
 
-export default LineItem
+export default LineItem;
